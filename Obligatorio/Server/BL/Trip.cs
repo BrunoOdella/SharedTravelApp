8ae64f99760b5d43d17a21317ec5e86acf116822
Seba <sebaesc01@gmail.com>
2024-03-24 17:01:32 -0300

    Created Business Logic folder and initial implementation of User and Trip classes

---
+++ Obligatorio/Server/BL/Trip.cs
@@ -0,0 +1,26 @@
+﻿namespace Server.BL;
+
+public class Trip
+{
+    private int _id;
+    private int _owner;
+    private List<int> _passengers;
+
+    public string Origin { get; set; }
+    public string Destination { get; set; }
+    public DateTime Departure { get; set; }
+    public int AvailableSeats { get; set; } = 0;
+    public int TotalSeats { get; set; } = 0;
+    public float PricePerPassanger { get; set; } = 0f;
+    public bool Pet { get; set; }
+    public string Photo { get; set; }
+
+    public Trip()
+    {
+        this._id = 0; //generar un numero acorde a algo
+        this._owner = 0;//generar un numero acorde a algo
+        this._passengers = new List<int>();
+    }
+
+
+}
---
+++ Obligatorio/Server/BL/User.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server.BL
+{
+    public class User
+    {
+        private int _id;
+
+        public string Name { get; set; }
+        private string _password;
+        public List<int> Trip;
+    }
+}
