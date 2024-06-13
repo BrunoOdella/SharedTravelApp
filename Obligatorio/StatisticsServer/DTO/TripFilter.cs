18360e0eadac9311ef0eb783a176835fa6f510f0
paulacantera <paulacanteramontero@hotmail.com>
2024-06-12 22:51:26 -0300

    added trip filter dto and trip repository

---
+++ Obligatorio/StatisticsServer/DTO/TripFilter.cs
@@ -0,0 +1,10 @@
+﻿namespace StatisticsServer.DTO
+{
+    public class TripFilter
+    {
+        public string Destination { get; set; }
+        public decimal? MaxPrice { get; set; }
+        public DateTime? StartDate { get; set; }
+        public DateTime? EndDate { get; set; }
+    }
+}
---
+++ Obligatorio/StatisticsServer/Repositories/TripRepository.cs
@@ -0,0 +1,35 @@
+﻿using StatisticsServer.DTO;
+
+namespace StatisticsServer.Repositories
+{
+    public interface ITripRepository
+    {
+        void Add(Trip trip);
+        IEnumerable<Trip> GetAll();
+        IEnumerable<Trip> GetFilteredTrips(TripFilter filter);
+    }
+
+    public class TripRepository : ITripRepository
+    {
+        private readonly List<Trip> _trips = new List<Trip>();
+
+        public void Add(Trip trip)
+        {
+            _trips.Add(trip);
+        }
+
+        public IEnumerable<Trip> GetAll()
+        {
+            return _trips;
+        }
+
+        public IEnumerable<Trip> GetFilteredTrips(TripFilter filter)
+        {
+            return _trips.Where(t =>
+                (string.IsNullOrEmpty(filter.Destination) || t.Destination == filter.Destination) &&
+                /*(!filter.MaxPrice.HasValue || t.PricePerPassanger <= filter.MaxPrice.Value) &&*/
+                (!filter.StartDate.HasValue || t.Departure >= filter.StartDate.Value) &&
+                (!filter.EndDate.HasValue || t.Departure <= filter.EndDate.Value));
+        }
+    }
+}
