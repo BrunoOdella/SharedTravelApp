7d2b2d12957b60e0f747e37cd5c7c65dece3fc0e
paulacantera <paulacanteramontero@hotmail.com>
2024-06-12 22:56:08 -0300

    add trip controller and filter trip controller

---
+++ Obligatorio/StatisticsServer/Controllers/TripController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StatisticsServer.DTO;
+using StatisticsServer.Repositories;
+
+namespace StatisticsServer.Controllers
+{
+    [ApiController]
+    [Route("api/trips")]
+    public class TripsController : ControllerBase
+    {
+        private readonly ITripRepository _tripRepository;
+
+        public TripsController(ITripRepository tripRepository)
+        {
+            _tripRepository = tripRepository;
+        }
+
+        [HttpPost]
+        public IActionResult AddTrip([FromBody] Trip trip)
+        {
+            _tripRepository.Add(trip);
+            return Ok();
+        }
+
+        [HttpGet("filter")]
+        public IActionResult GetFilteredTrips([FromQuery] TripFilter filter)
+        {
+            var trips = _tripRepository.GetFilteredTrips(filter);
+            return Ok(trips);
+        }
+    }
+}
+}
