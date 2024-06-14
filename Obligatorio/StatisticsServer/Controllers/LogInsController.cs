b274c12f4dcf78e85db712045acf09c102f294b1
paulacantera <paulacanteramontero@hotmail.com>
2024-06-14 15:55:11 -0300

    logins controller

---
+++ Obligatorio/StatisticsServer/Controllers/LogInsController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StatisticsServer.Repositories;
+
+namespace StatisticsServer.Controllers
+{
+    [ApiController]
+    [Route("api/logins")]
+    public class LogInsController : ControllerBase
+    {
+        private readonly ILoginEventRepository _loginEventRepository;
+
+        public LogInsController(ILoginEventRepository loginEventRepository)
+        {
+            _loginEventRepository = loginEventRepository;
+        }
+
+        [HttpGet]
+        public IActionResult GetAllLoginEvents()
+        {
+            return Ok(_loginEventRepository.GetAll());
+        }
+    }
+}
