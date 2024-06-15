9c28a9e4a0846c59932fcc88099a451abfd492bc
paulacantera <paulacanteramontero@hotmail.com>
2024-06-15 12:11:44 -0300

    deleted unnecessary classes

--- Obligatorio/StatisticsServer/DTO/Trip.cs
+++ Obligatorio/StatisticsServer/DTO/Trip.cs
@@ -3,22 +3,15 @@
     public class Trip
     {
         public Guid Id { get; set; }
-        public Guid Owner { get; set; }
-        public List<Guid> Passengers { get; set; }
-
         public string Origin { get; set; }
         public string Destination { get; set; }
         public DateTime Departure { get; set; }
         public int AvailableSeats { get; set; }
-        public int TotalSeats { get; set; }
         public float PricePerPassanger { get; set; }
-        public bool Pet { get; set; }
-        public string Photo { get; set; }
 
         public Trip()
         {
             this.Id = Guid.NewGuid();
-            this.Passengers = new List<Guid>();
         }
 
         internal void SetGuid(Guid tripId)
